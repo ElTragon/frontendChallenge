605b16ccc379a04e5da49731b9e0dcfabbbf9e91
Mario Lopez <mariodolt@marios-mbp.lan>
2023-07-30 21:44:56 -0700

    some refactoring and add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      footerText: "#d9dbe1",
+      black: "#201b21",
+      darkGrey: "#67696e",
+      lineLightGrey: "#e9ebee",
+      paleGreen: "#a7c6ba",
+      orange: "#ec5e2a",
+      white: "#fffffff",
+      heroBg: "#eaeeef",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
