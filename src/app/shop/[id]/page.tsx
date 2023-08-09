e3e5e2ad2c49ee628e3531180ddc8c87bf477336
Mario Lopez <mariodolt@marios-mbp.lan>
2023-08-09 15:55:40 -0700

    update text

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -35,7 +35,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <Layout
       route={""}
-      title={"Homepage"}
+      title={"ShopItem"}
       description={"Creased Jordans, we got you"}
     >
       <ProductProvider>
